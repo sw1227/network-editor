bd3eb5ee77913b56103f85a5fe7fa324127114cf
sw1227 <32032108+sw1227@users.noreply.github.com>
2021-12-29 17:30:56 +0900

    feat: update circle layer when clicked on map

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
-import mapboxgl, { MapboxOptions } from 'mapbox-gl'
+import { useEffect, useState } from 'react'
+import mapboxgl, { MapboxOptions, GeoJSONSource } from 'mapbox-gl'
+import { FeatureCollection } from 'geojson'
 import styles from '../styles/Map.module.css'
 
 const options: MapboxOptions = {
@@ -11,11 +12,65 @@ const options: MapboxOptions = {
   zoom: 12
 }
 
-const Map: NextPage = () => {
+const nodesToGeoJson = (nodes: mapboxgl.LngLat[]): FeatureCollection => {
+  return {
+    type: 'FeatureCollection',
+    features: nodes.map(node => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [node.lng, node.lat]
+      },
+      properties: []
+    }))
+  }
+}
 
+const Map: NextPage = () => {
+  // States
+  const [nodes, setNodes] = useState<mapboxgl.LngLat[]>([])
+  const [map, setMap] = useState<mapboxgl.Map>()
+
+  // Create map instance on initial render
   useEffect(() => {
     const map = new mapboxgl.Map(options)
+    setMap(map)
   }, [])
+
+  // Add source and event listener to the map
+  useEffect(() => {
+    if (!map) return
+    map.on('style.load', () => {
+      map.on('click', e => {
+        setNodes(prev => [...prev, e.lngLat])
+      })
+      map.addSource('nodes', {
+        type: 'geojson',
+        data: nodesToGeoJson(nodes)
+      })
+    })
+  }, [map])
+
+  // Update layer according to the nodes
+  useEffect(() => {
+    const nodesSource = map?.getSource('nodes') as GeoJSONSource
+    if (!map || !nodesSource) return
+    nodesSource.setData(nodesToGeoJson(nodes))
+
+    if (map.getLayer('nodes')) map.removeLayer('nodes')
+    const nodesLayer: mapboxgl.CircleLayer = {
+      id: 'nodes',
+      type: 'circle',
+      source: 'nodes',
+      layout: {},
+      paint: {
+        'circle-radius': 6,
+        'circle-color': '#f08',
+        'circle-opacity': 0.4
+      }
+    }
+    map.addLayer(nodesLayer)
+}, [nodes])
 
   return (
     <div id="mapbox" className={styles.mapbox}></div>
