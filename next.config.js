8fe64c074891edede219fecbdd94307d1578738b
sw1227 <32032108+sw1227@users.noreply.github.com>
2022-03-06 20:00:25 +0900

    feat: add workflow to deploy to GitHub pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/network-editor" : "",
+  trailingSlash: true,
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 }
